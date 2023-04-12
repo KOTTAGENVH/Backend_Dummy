e3c61b220a48d5794f8602558706a3c7b30799ee
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-04-12 19:40:27 +0530

    Update server.js

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,1 +1,24 @@
 
+// const express = require("express");
+const gateway = require("fast-gateway");
+
+const port = 9001;
+const server = gateway({
+    routes: [
+
+        {
+            prefix: "/user",
+            target: "http://localhost:4000/",
+            hooks: {}
+        }
+    ]
+});
+
+server.get('/mytesting', (req,res)=> {
+    res.send("Gateway Called");
+})
+
+server.start(port).then(server=>{
+    console.log("Gateway is running "+port);
+})
+
